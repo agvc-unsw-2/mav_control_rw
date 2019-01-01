36f8772003bc7e004c042466bebfd42cecdae14b
HideyoshiCheong <hideyoshi@live.com.au>
2019-01-01 19:27:39 +1100

    Updated stuff

--- launch_bridges.sh
+++ launch_bridges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xterm -hold -e roslaunch rotors_gazebo joy_to_roll_pitch_yawrate_thrust.launch mav_name:=ardrone &
+xterm -hold -e roslaunch rotors_gazebo joy_to_roll_pitch_yawrate_thrust.launch mav_name:=firefly &
 
-xterm -hold -e python sim_bridges/mav_actuators_to_float64.py &
+xterm -hold -e python sim_bridges/mav_actuators_to_float64.py

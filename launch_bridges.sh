e2a94256a3d6bca15ba1237ea1c549d3e567ff92
mtrnlab1 <mtrnlab1@ucrg.cc>
2019-01-05 16:43:16 +1100

    Testing stuff

--- launch_bridges.sh
+++ launch_bridges.sh
@@ -7,6 +7,7 @@ fi
 
 mav_name=$1
 
-xterm -hold -e roslaunch rotors_gazebo joy_to_roll_pitch_yawrate_thrust.launch mav_name:=$mav_name &
+#xterm -hold -e roslaunch rotors_gazebo joy_to_roll_pitch_yawrate_thrust.launch mav_name:=$mav_name &
 
+xterm -hold -e roslaunch mav_linear_mpc joy_publisher.launch mav_name:=$mav_name & 
 xterm -hold -e python sim_bridges/mav_actuators_to_float64.py $mav_name

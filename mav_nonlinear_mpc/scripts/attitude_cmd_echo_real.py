c4ad49eaf3e7ac73673ad337b8a7068a899ba375
HideyoshiCheong <hideyoshi@live.com.au>
2019-03-09 10:57:21 +1100

    Added more scripts for scaling and echoing stuff for ss id

--- mav_nonlinear_mpc/scripts/attitude_cmd_echo_real.py
+++ mav_nonlinear_mpc/scripts/attitude_cmd_echo_real.py
@@ -5,6 +5,7 @@ import sys
 import rospy
 from std_msgs.msg import Time
 from mav_msgs.msg import RollPitchYawrateThrust
+from sensor_msgs.msg import Imu
 import std_msgs.msg
 
 class Echo_From_Vrep(object):
@@ -13,10 +14,11 @@ class Echo_From_Vrep(object):
         self.input_delay = rospy.Duration(input_delay)
         self.updated = False
         self.msg_to_publish = RollPitchYawrateThrust()
-        self.pub = rospy.Publisher("/simulation/uav" + uav_num + "/command/roll_pitch_yawrate_thrust", RollPitchYawrateThrust, queue_size=1)
+        self.pub = rospy.Publisher('/' + mav_name + uav_num + '/command/roll_pitch_yawrate_thrust_sysid', RollPitchYawrateThrust, queue_size=1)
 
+        #rospy.Subscriber('/mavros/imu/data', Imu, self.write_callback)
         rospy.Subscriber('/sim_time', Time, self.write_callback)
-        rospy.Subscriber('/' + mav_name + '/command/roll_pitch_yawrate_thrust', RollPitchYawrateThrust, self.read_callback)
+        rospy.Subscriber('/' + mav_name + uav_num + '/command/roll_pitch_yawrate_thrust_scaled', RollPitchYawrateThrust, self.read_callback)
 
     def read_callback(self, msg):
         #print(data.angular_velocities)

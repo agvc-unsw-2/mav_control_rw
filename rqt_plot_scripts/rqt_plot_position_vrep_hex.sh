970645ba074539281ff80f66978b09da4d4bead6
Hideyoshi Cheong <hideyoshi@live.com.au>
2019-03-30 20:17:25 +1100

    Added rqt plot scripts and circle path generating script

---
+++ rqt_plot_scripts/rqt_plot_position_vrep_hex.sh
@@ -0,0 +1,1 @@
+rqt_plot /simulation/uav1/ground_truth/mavros/position/local/pose/pose/position/x:y /vrep_hex1/command/trajectory/points[0]/transforms[0]/translation/x:y
---
+++ rqt_plot_scripts/rqt_plot_velocities_vrep_hex.sh
@@ -0,0 +1,1 @@
+rqt_plot /simulation/uav1/ground_truth/mavros/position/local/twist/twist/linear/x:y /vrep_hex1/command/trajectory/points[0]/velocities[0]/linear/x:y

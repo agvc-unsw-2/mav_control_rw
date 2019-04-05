77555c8e8a21861eb5b9b0873920b2c1f4caa8da
HideyoshiCheong <hideyoshi@live.com.au>
2019-04-06 10:25:56 +1100

    Updated circle path publisher to take sim time with roslaunch file. Needs testing

--- mav_nonlinear_mpc/scripts/publish_full_circle_path_sim.py
+++ mav_nonlinear_mpc/scripts/publish_full_circle_path_sim.py
@@ -17,8 +17,9 @@ import std_msgs.msg
 from math import cos
 from math import sin
 
-class Publish_Path(object):
-    def __init__(self, mav_name, uav_num, circle_r, altitude, vel_mag, publish_interval, ref_time_step):
+class Path_Publisher(object):
+    def __init__(self, mav_name, uav_num, circle_r, altitude, vel_mag, ref_time_step, publish_interval):
+        rospy.init_node('publish_full_circle_path_sim' + uav_num, anonymous=False)
         self.pub = rospy.Publisher(
             '/' + mav_name + uav_num + '/command/trajectory', MultiDOFJointTrajectory, queue_size=1
         )
@@ -32,49 +33,54 @@ class Publish_Path(object):
         self.traj_msg = MultiDOFJointTrajectory()
         self.N = int(publish_interval / ref_time_step) * 2
         self.traj_msg.points = [None]*self.N
+        self.rate = rospy.Rate(1.0/publish_interval)
 
         for i in range(self.N):
             self.traj_msg.points[i] = MultiDOFJointTrajectoryPoint()
 
         #rospy.Subscriber('/mavros/imu/data', Imu, self.write_callback)
-        rospy.Subscriber('/sim_time', Time, self.write_callback)
     
-    def write_callback(self, msg):
-        t_read = msg.data.secs + msg.data.nsecs * 1e-9 # t_read in seconds
-        #print(t_read)
-        if self.t0 == -1:
-            self.t0 = t_read
-        t_since_start = t_read - self.t0
-        if t_since_start < 0:
-            self.t0 = t_read
-            t_since_start = 0.0
+    def publish_path(self):
         d = self.circle_r
         mu = self.vel_scale_factor
-
         zero_vec = Vector3(0.0, 0.0, 0.0)
-        #roll = 0
-        #pitch = 0
-        #yaw = math.atan2(vel_ref[1],vel_ref[0])
-        #quat = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-        rotation = Quaternion(0.0, 0.0, 0.0, 1.0)
-        if t_since_start > (self.t_last_published + (self.publish_interval)): #publish every 100ms
-            # generate 2 * points required before next publish interval
-            for i in range(self.N):
-                t = t_since_start + (i * self.ref_time_step)
-                p_ref = Vector3(d*cos(mu*t), d*sin(mu*t), self.altitude)
-                v_ref = Vector3(-mu*d*sin(mu*t), mu*d*cos(mu*t), 0.0)
-                a_ref = Vector3(-mu**2*d*cos(mu*t), -mu**2*d*sin(mu*t), 0.0)
-                #rotation = Quaternion(quat[0], quat[1], quat[2], quat[3])
-                vel_ang = zero_vec
-                acc_ang = zero_vec
-                self.traj_msg.points[i].transforms = [Transform(p_ref, rotation)]
-                self.traj_msg.points[i].velocities = [Twist(v_ref, vel_ang)]
-                self.traj_msg.points[i].accelerations = [Twist(a_ref, acc_ang)]
-                self.traj_msg.points[i].time_from_start = rospy.Time.from_sec(t)
+        vel_ang = zero_vec
+        acc_ang = zero_vec
+        while not rospy.is_shutdown():
+            now_secs = rospy.get_time()
+            now_obj = rospy.get_rostime()
+            if now_secs == 0: # simulation hasn't started
+                print(now_secs)
+            else:
                 
-            self.traj_msg.header.stamp = msg.data
-            self.pub.publish(self.traj_msg)
-            self.t_last_published = t_since_start
+                print(now_secs)
+                if self.t0 == -1:
+                    self.t0 = now_secs
+                t_since_start = now_secs - self.t0
+                if t_since_start < 0:
+                    self.t0 = now_secs
+                    t_since_start = 0.0
+                rotation = Quaternion(0.0, 0.0, 0.0, 1.0)
+                # generate 2 * points required before next publish interval
+                for i in range(self.N):
+                    t = t_since_start + (i * self.ref_time_step)
+                    p_ref = Vector3(d*cos(mu*t), d*sin(mu*t), self.altitude)
+                    v_ref = Vector3(-mu*d*sin(mu*t), mu*d*cos(mu*t), 0.0)
+                    a_ref = Vector3(-mu**2*d*cos(mu*t), -mu**2*d*sin(mu*t), 0.0)
+                    #roll = 0
+                    #pitch = 0
+                    #yaw = math.atan2(v_ref[1],v_ref[0])
+                    #quat = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+                    #rotation = Quaternion(quat[0], quat[1], quat[2], quat[3])
+                    self.traj_msg.points[i].transforms = [Transform(p_ref, rotation)]
+                    self.traj_msg.points[i].velocities = [Twist(v_ref, vel_ang)]
+                    self.traj_msg.points[i].accelerations = [Twist(a_ref, acc_ang)]
+                    self.traj_msg.points[i].time_from_start = rospy.Time.from_sec(t)
+                    
+                self.traj_msg.header.stamp = now_obj
+                self.pub.publish(self.traj_msg)
+                self.t_last_published = t_since_start
+            self.rate.sleep() #wait until next time instance
 
 myargs = rospy.myargv(argv=sys.argv)
 if len(myargs) != 3:
@@ -87,19 +93,21 @@ uav_num = str(myargs[2])
 if __name__ == '__main__':
     print('-----------------------')
     print("Launching " + myargs[0] + '...')
-    rospy.init_node('attitude_cmd_echo_' + uav_num, anonymous=False)
     circle_r = 5.0
     altitude = 1.0
     vel_mag = 2.0
     publish_interval = 5.0 #s
     ref_time_step = 0.01 #s
-    publisher_ = Publish_Path(
+    publisher_obj = Path_Publisher(
         mav_name, 
         uav_num, 
         circle_r, 
         altitude, 
         vel_mag, 
-        publish_interval,
-        ref_time_step
+        ref_time_step,
+        publish_interval
     )
-    rospy.spin()
+    try:
+        publisher_obj.publish_path()
+    except rospy.ROSInterruptException:
+        pass

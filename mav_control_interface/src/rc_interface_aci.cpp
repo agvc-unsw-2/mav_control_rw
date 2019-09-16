9bf7cc21f005061cec1f322126cd18ef44596882
mtrnlab5 <unswmbzirc@gmail.com>
2019-09-16 10:36:44 +1000

    Disabled MANUAL OFFBOARD mode

--- mav_control_interface/src/rc_interface_aci.cpp
+++ mav_control_interface/src/rc_interface_aci.cpp
@@ -39,7 +39,7 @@ void RcInterfaceAci::rcCallback(const sensor_msgs::JoyConstPtr& msg)
     last_data_.left_up_down = msg->axes[2];
     //last_data_.left_side = -msg->axes[3];
     last_data_.left_side = msg->axes[3]; // reverse yaw
-
+    /*
     if (msg->axes[5] > 0.0)
     {
       last_data_.control_interface = RcData::ControlInterface::OFF;
@@ -49,12 +49,13 @@ void RcInterfaceAci::rcCallback(const sensor_msgs::JoyConstPtr& msg)
     }
     else
     {
+      */
       last_data_.control_interface = RcData::ControlInterface::ON;
       last_data_.control_mode = RcData::ControlMode::POSITION_CONTROL;
 
       //ROS_WARN_STREAM_THROTTLE(1.0, "CONTROL INTERFACE OFF");
       ROS_WARN_STREAM_THROTTLE(5.0, "POSITION OFFBOARD MODE");
-    }
+    //}
 
     /*
     if (msg->axes[4] <= -0.5)

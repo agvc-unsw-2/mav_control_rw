e3b62b8ef309c8facd5efd74fb9319a0051e08af
hoshi <hideyoshi@live.com.au>
2019-12-15 15:20:18 +1100

    fixed compile error in delay command simulator and added launch file

--- mav_control_interface/src/delay_command.cpp
+++ mav_control_interface/src/delay_command.cpp
@@ -4,14 +4,14 @@
 DelayCommand::DelayCommand(const ros::NodeHandle& nh, const ros::NodeHandle& private_nh, int elems_to_delay)
     : nh_(nh),
       private_nh_(private_nh),
-      current_queue_size_(0),
-      elems_to_delay_(elems_to_delay)
+      current_queue_size_(0)
 {
     seq_ref_.clear();
     roll_ref_.clear();
     pitch_ref_.clear();
     yaw_rate_ref_.clear();
     thrust_z_ref_.clear();
+    elems_to_delay_ = elems_to_delay;
     chatter_sub_ = nh_.subscribe("/mavros/setpoint_raw/roll_pitch_yawrate_thrust", 1, &DelayCommand::publishCb, this);
     chatter_pub_ = nh_.advertise<mav_msgs::RollPitchYawrateThrust>("/mavros/setpoint_raw/roll_pitch_yawrate_thrust_delayed", 1);
 }
@@ -43,6 +43,7 @@ void DelayCommand::publishCb(const mav_msgs::RollPitchYawrateThrust::Ptr& msg_in
         current_queue_size_--;
         chatter_pub_.publish((*msg_in));
     }
+    //ROS_INFO_STREAM("current_queue_size: " << current_queue_size_);
 }
 
 int main(int argc, char **argv)
@@ -55,8 +56,11 @@ int main(int argc, char **argv)
         return 1;
     }
     double msg_freq = std::stod(argv[1]);
-    double delay_time = std::stod(argv[1]);
+    double delay_time = std::stod(argv[2]);
+    ROS_WARN_STREAM_ONCE("msg_freq: " << msg_freq);
+    ROS_WARN_STREAM_ONCE("delay_time: " << delay_time);
     int elems_to_delay = (int)(msg_freq * delay_time);
+    ROS_WARN_STREAM_ONCE("elems_to_delay: " << elems_to_delay);
     DelayCommand delay_command_obj(nh, pnh, elems_to_delay);
 
     ros::spin();
